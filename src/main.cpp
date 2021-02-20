301ec3088a63bc3d1a462211593b47128fdffcd0
Jan Kolena <jendakolena@gmail.com>
2021-02-20 17:40:01 +0100

    Sleep, wake up on button, send data and repeat

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,39 @@
 #include <Arduino.h>
+#include <LowPower.h>
 #include <RF24.h>
 #include "printf.h"
 
 #define AG_PIN_CE 10
 #define AG_PIN_CSN 9
-#define AG_PIN_BUTT 12
+#define AG_PIN_BUTT 3
 
 RF24 radio(AG_PIN_CE, AG_PIN_CSN);
 
 const byte address[6] = "1tran";
 
-void initRadio() {
+void wakeUp() {
+    // no-op
+}
+
+bool initRadio() {
     if (!radio.begin()) {
         Serial.println("Could not initialize radio!!!");
-        for (int i = 0; i < 5; i++) {
-            delay(200);
-        }
-    } else Serial.println("Radio initialized");
+        return false;
+    }
+
+    Serial.println("Radio initialized");
+
+    radio.setAutoAck(true);
+    radio.enableDynamicPayloads();
+    radio.setDataRate(RF24_250KBPS);
+    radio.setRetries(15, 15);
+    radio.setPALevel(RF24_PA_MAX);
+    radio.setChannel(109);
+    radio.openWritingPipe(address);
+    radio.stopListening();
+    radio.printDetails();
+
+    return true;
 }
 
 void setup() {
@@ -29,34 +46,35 @@ void setup() {
 
     printf_begin();
 
-    initRadio();
-    radio.setAutoAck(true);
-    radio.enableDynamicPayloads();
-    radio.setDataRate(RF24_250KBPS);
-    radio.setRetries(15, 15);
-    radio.setPALevel(RF24_PA_MAX);
-    radio.setChannel(109);
-    radio.openWritingPipe(address);
-    radio.stopListening();
-    radio.printDetails();
+    if (initRadio()) {
+        Serial.println("Started!");
+        delay(300); // to complete the output
+    } else {
+        // TODO
+    }
 }
 
 void loop() {
-    bool pressed = digitalRead(AG_PIN_BUTT) == 0;
+    // Allow wake up pin to trigger interrupt on low.
+    attachInterrupt(1, wakeUp, LOW);
+    // Enter power down state with ADC and BOD module disabled.
+    // Wake up when wake up pin is low.
+    LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+    delay(50); // wait for the sleep
 
-    if (pressed) {
-        char concatenated_string[31];
+    // Disable external pin interrupt on wake up pin.
+    detachInterrupt(1);
 
-        sprintf(concatenated_string, "t=%lu", millis());
+    char concatenated_string[31];
 
-        Serial.print("Sending data: ");
-        Serial.println(concatenated_string);
+    sprintf(concatenated_string, "t=%lu", millis());
 
+    Serial.print("Sending data: ");
+    Serial.println(concatenated_string);
 
-        if (!radio.write(concatenated_string, sizeof(concatenated_string))) {
-            Serial.println("Could not sent data!");
-        } else Serial.println("ACK");
+    if (!radio.write(concatenated_string, sizeof(concatenated_string))) {
+        Serial.println("Could not sent data!");
+    } else Serial.println("ACK");
 
-        delay(100);
-    }
+    delay(100);
 }
