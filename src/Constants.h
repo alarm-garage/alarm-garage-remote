e1674ab607a72729569802fd97177e122cb6e860
Jan Kolena <jendakolena@gmail.com>
2021-03-05 00:31:23 +0100

    Rolling code and encryption
    Sending rolling code to receiver.
    Saving state and settings in EEPROM.

---
+++ src/Constants.h
@@ -0,0 +1,15 @@
+#ifndef ALARM_GARAGE_CONSTANTS_H
+#define ALARM_GARAGE_CONSTANTS_H
+
+#define AG_PIN_CE 10
+#define AG_PIN_CSN 9
+#define AG_PIN_BUTT 3
+
+#define AG_SIGNAL_AUTH_SIZE 4
+#define AG_SIGNAL_DATA_SIZE 14
+#define AG_SIGNAL_AUTH_TAG_SIZE 8
+#define AG_SIGNAL_KEY_SIZE 16
+#define AG_SIGNAL_IV_SIZE 16
+#define AG_SIGNAL_IV {0x91, 0x5f, 0xf8, 0xff, 0xca, 0xd8, 0xae, 0x1d, 0xf4, 0x45, 0xeb, 0x03, 0xe2, 0x18, 0xfd, 0x25}
+
+#endif //ALARM_GARAGE_CONSTANTS_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,31 @@
 #include <Arduino.h>
 #include <LowPower.h>
+#include <EEPROM.h>
+
 #include <RF24.h>
 #include "printf.h"
 
-#define AG_PIN_CE 10
-#define AG_PIN_CSN 9
-#define AG_PIN_BUTT 3
+#include <Crypto.h>
+#include <Ascon128.h>
+#include "Constants.h"
+
+#include <proto/pb_encode.h>
+#include <proto/alarm.pb.h>
 
 RF24 radio(AG_PIN_CE, AG_PIN_CSN);
+Ascon128 cipher;
 
 const byte address[6] = "1tran";
+const byte iv[AG_SIGNAL_IV_SIZE] = AG_SIGNAL_IV;
+
+typedef struct ClientState {
+    char clientId[5];
+    byte key[AG_SIGNAL_KEY_SIZE];
+    byte authdata[AG_SIGNAL_AUTH_SIZE];
+    uint32_t lastCode;
+} ClientState;
+
+ClientState clientState;
 
 void wakeUp() {
     // no-op
@@ -25,6 +41,7 @@ bool initRadio() {
 
     radio.setAutoAck(true);
     radio.enableDynamicPayloads();
+    radio.setPayloadSize(32);
     radio.setDataRate(RF24_250KBPS);
     radio.setRetries(15, 15);
     radio.setPALevel(RF24_PA_MAX);
@@ -36,12 +53,82 @@ bool initRadio() {
     return true;
 }
 
+bool encrypt_signal(_protocol_RemoteSignal *output,
+                    const byte authData[AG_SIGNAL_AUTH_SIZE],
+                    const _protocol_RemoteSignalPayload payload) {
+    cipher.clear();
+
+    uint8_t pb_buffer[16];
+    pb_ostream_t stream = pb_ostream_from_buffer(pb_buffer, sizeof(pb_buffer));
+
+    if (!pb_encode(&stream, protocol_RemoteSignalPayload_fields, &payload)) {
+        Serial.println("Could not encode the signal payload!");
+        return false;
+    }
+
+    Serial.print("Signal payload encoded into ");
+    size_t bytes = stream.bytes_written;
+    Serial.print(bytes);
+    Serial.print(" bytes\n");
+
+    if (!cipher.setKey(clientState.key, AG_SIGNAL_KEY_SIZE)) {
+        Serial.print("setKey ");
+        return false;
+    }
+    if (!cipher.setIV(iv, AG_SIGNAL_IV_SIZE)) {
+        Serial.print("setIV ");
+        return false;
+    }
+
+    cipher.addAuthData(authData, AG_SIGNAL_AUTH_SIZE);
+    cipher.encrypt(output->payload.bytes, pb_buffer, AG_SIGNAL_DATA_SIZE);
+    output->payload.size = AG_SIGNAL_DATA_SIZE;
+    cipher.computeTag(output->auth_tag.bytes, AG_SIGNAL_AUTH_TAG_SIZE);
+    output->auth_tag.size = AG_SIGNAL_AUTH_TAG_SIZE;
+
+    return true;
+}
+
+void sendSignal() {
+    _protocol_RemoteSignal signal = protocol_RemoteSignal_init_zero;
+
+    strcpy(signal.client_id, clientState.clientId);
+
+    _protocol_RemoteSignalPayload payload = protocol_RemoteSignalPayload_init_zero;
+    payload.code = ++clientState.lastCode;
+    strcpy(reinterpret_cast<char *>(payload.random.bytes), "abcdefghij");
+    payload.random.size = 10;
+
+    if (!encrypt_signal(&signal, clientState.authdata, payload)) {
+        Serial.println("Could not encrypt signal payload!");
+        return;
+    }
+
+    printf("Sending code %d\n", clientState.lastCode);
+
+    uint8_t pb_buffer[64];
+    pb_ostream_t stream = pb_ostream_from_buffer(pb_buffer, sizeof(pb_buffer));
+
+    if (!pb_encode(&stream, protocol_RemoteSignal_fields, &signal)) {
+        Serial.println("Could not encode the signal!");
+        return;
+    }
+
+    size_t bytes = stream.bytes_written;
+
+    EEPROM.put(0, clientState);
+
+    if (!radio.write(pb_buffer, bytes)) {
+        Serial.println("Could not sent data!");
+    } else Serial.println("ACK");
+}
+
 void setup() {
     Serial.begin(115200);
     delay(200);
     Serial.println("\n---\nStarting...\n");
 
-    pinMode(AG_PIN_BUTT, INPUT);
+    pinMode(AG_PIN_BUTT, INPUT_PULLUP);
     digitalWrite(AG_PIN_BUTT, HIGH);
 
     printf_begin();
@@ -50,11 +137,17 @@ void setup() {
         Serial.println("Started!");
         delay(300); // to complete the output
     } else {
-        // TODO
+        // TODO display
+        return;
     }
+
+    EEPROM.get(0, clientState);
 }
 
 void loop() {
+    // TODO make this much better!
+    while (digitalRead(AG_PIN_BUTT) == LOW); // prevent sending two events
+
     // Allow wake up pin to trigger interrupt on low.
     attachInterrupt(1, wakeUp, LOW);
     // Enter power down state with ADC and BOD module disabled.
@@ -65,16 +158,7 @@ void loop() {
     // Disable external pin interrupt on wake up pin.
     detachInterrupt(1);
 
-    char concatenated_string[31];
-
-    sprintf(concatenated_string, "t=%lu", millis());
-
-    Serial.print("Sending data: ");
-    Serial.println(concatenated_string);
-
-    if (!radio.write(concatenated_string, sizeof(concatenated_string))) {
-        Serial.println("Could not sent data!");
-    } else Serial.println("ACK");
+    sendSignal();
 
     delay(100);
 }
